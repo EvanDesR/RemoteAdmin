01c310e174af3b418d908f3766ef8272a5306115
EvanDesR <132327585+EvanDesR@users.noreply.github.com>
2024-10-27 13:15:33 -0400

    needed.h added

---
+++ needed.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <ws2def.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#pragma comment(lib, "ws2_32.lib") //winsock library file
